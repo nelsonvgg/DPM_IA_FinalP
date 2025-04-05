4a687217192f99d574fd88dc0ce127b25f56593a
nelsonvgg <nelson.granda@epn.edu.ec>
2025-04-05 09:38:55 -0500

    Final

--- tests/run_tests_with_coverage.py
+++ tests/run_tests_with_coverage.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
         "--cov=.",  # Measure coverage for the entire project
         f"--cov-report=xml:{os.path.join('./tests', 'coverage.xml')}",  # Generate coverage report in XML format
         "--cov-report=term",  # Display coverage summary in the terminal
+        "--cov-report=html",  # Generate coverage report in HTML format
         "--cov-config=tests/.coveragerc",  # Use the coverage configuration file
         "tests/"  # Specify the tests directory
     ])
